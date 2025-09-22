84dce5b07ac32ff37dae882d886f7e3bb89c4c95
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-09-22 23:51:01 +0700

    1

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.frontend.index');
+});
+
+require __DIR__ . '/auth.php';
+require __DIR__ . '/backend/dashboard.php';
+require __DIR__ . '/backend/language.php';
+require __DIR__ . '/backend/administrative/application.php';
+require __DIR__ . '/backend/administrative/database.php';
+require __DIR__ . '/backend/administrative/management.php';
+require __DIR__ . '/backend/administrative/session.php';
+
+require __DIR__ . '/backend/application/datatable.php';
+
+Route::get('/templates', function () {
+    return view('layouts.template');
+});
