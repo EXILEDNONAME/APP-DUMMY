5bc047aa7beaf7714724316ff1479160de697f49
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-10-03 15:17:49 +0700

    14

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,120 +1,64 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <base href="../../../../">
+    <title> EXILEDNONAME - Register </title>
     <meta charset="utf-8" />
-    <title>
-        @php $title = Cache::remember('title', 300, function () { return \DB::table('system_settings')->first(); }); @endphp
-        {{ $title->application_name; }} - Register
-    </title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="Register">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700">
-    <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/login/login-5.css">
-    <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/login/style.bundle.min.css">
-    <link rel="shortcut icon" href="{{ env('APP_URL') }}/favicon.png" />
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport" />
+    <link href="/assets/backend/media/app/favicon.ico" rel="shortcut icon" />
+    <link href="/assets/backend/css/styles.css" rel="stylesheet" />
 </head>
 
-<body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
-
-    <div class="d-flex flex-column flex-root">
-        <div class="login login-5 login-signin-on d-flex flex-row-fluid" id="kt_login">
-            <div class="d-flex flex-center bgi-size-cover bgi-no-repeat flex-row-fluid" style="background-image: url({{ env('APP_URL') }}/assets/backend/media/bg/bg-2.jpg);">
-                <div class="login-form text-center text-white p-7 position-relative overflow-hidden">
-
-                    <div class="login-signin text-white">
-                        <div class="mb-10">
-                            <h3 class="opacity-40 font-weight-normal"> - REGISTER - </h3>
-                            <p class="opacity-40"> Enter Your Details Account </p>
-                        </div>
-
-                        <form class="form" method="POST" action="{{ route('register') }}">
-                            @csrf
-
-                            <div class="form-group">
-                                <input class="form-control h-auto text-white bg-white-o-5 rounded-pill border-0 py-4 px-8" type="text" placeholder="Name" name="name" value="{{ old('name') }}" required />
-                            </div>
-                            <div class="form-group">
-                                <input class="form-control h-auto text-white bg-white-o-5 rounded-pill border-0 py-4 px-8" type="email" placeholder="Email" name="email" value="{{ old('email') }}" required />
-                            </div>
-                            <div class="form-group">
-                                <input class="form-control h-auto text-white bg-white-o-5 rounded-pill border-0 py-4 px-8" type="number" placeholder="Phone" name="phone" value="{{ old('phone') }}" required />
-                            </div>
-                            <div class="form-group">
-                                <input class="form-control h-auto text-white bg-white-o-5 rounded-pill border-0 py-4 px-8" type="text" placeholder="Username" name="username" value="{{ old('username') }}" required />
-                            </div>
-                            <div class="form-group">
-                                <input class="form-control h-auto text-white bg-white-o-5 rounded-pill border-0 py-4 px-8" type="password" placeholder="Password" name="password" required />
-                            </div>
-                            <div class="form-group">
-                                <input class="form-control h-auto text-white bg-white-o-5 rounded-pill border-0 py-4 px-8" type="password" placeholder="Password Confirmation" name="password_confirmation" required />
-                            </div>
-
-                            <center>
-                                @error('name')
-                                <div class="fv-plugins-message-container mt-0">
-                                    <div data-field="name" class="fv-help-block"><strong>{{ $message }}</strong></div>
-                                </div><br>
-                                @enderror
-
-                                @error('email')
-                                <div class="fv-plugins-message-container mt-0">
-                                    <div data-field="email" class="fv-help-block"><strong>{{ $message }}</strong></div>
-                                </div><br>
-                                @enderror
-
-                                @error('phone')
-                                <div class="fv-plugins-message-container mt-0">
-                                    <div data-field="phone" class="fv-help-block"><strong>{{ $message }}</strong></div>
-                                </div><br>
-                                @enderror
-
-                                @error('username')
-                                <div class="fv-plugins-message-container mt-0">
-                                    <div data-field="username" class="fv-help-block"><strong>{{ $message }}</strong></div>
-                                </div><br>
-                                @enderror
-
-                                @error('password')
-                                <div class="fv-plugins-message-container mt-0">
-                                    <div data-field="password" class="fv-help-block"><strong>{{ $message }}</strong></div>
-                                </div><br>
-                                @enderror
-
-                                @error('password_confirmation')
-                                <div class="fv-plugins-message-container mt-0">
-                                    <div data-field="password_confirmation" class="fv-help-block"><strong>{{ $message }}</strong></div>
-                                </div><br>
-                                @enderror
-                            </center>
-
-                            @if ($message = Session::get('status'))
-                            <div id="toast-container" class="toast-bottom-right">
-                                <div class="toast toast-success" aria-live="polite">
-                                    <div class="toast-message">{{ $message }}</div>
-                                </div>
-                            </div>
-                            @endif
-
-                            <div class="form-group text-center d-flex flex-wrap justify-content-between align-items-center px-8 opacity-60">
-                                <div class="checkbox-inline">
-                                    <label class="checkbox checkbox-outline checkbox-white text-white m-0">
-                                        <input type="checkbox" name="remember" />
-                                        <span></span>
-                                        Remember me
-                                    </label>
-                                </div>
-                                <a href="{{ route('password.request') }}" class="text-white font-weight-bold">Forget Password ?</a>
-                            </div>
-                            <div class="form-group text-center mt-10">
-                                <button type="submit" class="btn btn-pill btn-primary opacity-90 px-15 py-3">Sign In</button>
-                            </div>
-                        </form>
+<body class="antialiased flex h-full text-base text-foreground bg-background">
+    <div class="grid lg:grid-cols-1 grow">
+        <div class="flex justify-center items-center p-8 lg:p-10 order-2 lg:order-1">
+            <div class="kt-card max-w-[370px] w-full">
+                <form action="#" class="kt-card-content flex flex-col gap-5 p-10" id="sign_up_form" method="post">
+                    <div class="text-center mb-2.5">
+                        <h3 class="text-lg font-medium text-mono leading-none mb-2.5">
+                            - REGISTER -
+                        </h3>
+                    </div>
+                    <div class="grid grid-cols-1 gap-2.5">
+                        <a class="kt-btn kt-btn-outline justify-center" href="#">
+                            <img alt="" class="size-3.5 shrink-0" src="/assets/backend/media/brand-logos/google.svg" />
+                            Use Google
+                        </a>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <span class="border-t border-border w-full"></span>
+                        <span class="text-xs text-secondary-foreground uppercase"> or </span>
+                        <span class="border-t border-border w-full"></span>
                     </div>
 
-                </div>
+                    <div class="flex flex-col gap-1">
+                        <input class="kt-input" name="email" placeholder="Name" type="text" value="" title="TEST" />
+                    </div>
+
+                    <div class="flex flex-col gap-1">
+                        <input class="kt-input" name="email" placeholder="Email" type="text" value="" />
+                    </div>
+
+                    <div class="flex flex-col gap-1">
+                        <input class="kt-input" name="email" placeholder="Phone" type="text" value="" />
+                    </div>
+
+                    <div class="flex flex-col gap-1">
+                        <input class="kt-input" name="email" placeholder="Username" type="text" value="" />
+                    </div>
+
+                    <div class="flex flex-col gap-1">
+                        <input class="kt-input" name="email" placeholder="Password" type="password" value="" />
+                    </div>
+
+                    <div class="flex flex-col gap-1">
+                        <input class="kt-input" name="email" placeholder="Confirm Password" type="password" value="" />
+                    </div>
+
+                    <button class="kt-btn kt-btn-primary flex justify-center grow">
+                        Sign up
+                    </button>
+                </form>
             </div>
         </div>
     </div>
